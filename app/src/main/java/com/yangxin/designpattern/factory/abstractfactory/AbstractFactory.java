7bdd4aa41ad831af4289dc9fb83e3f1bcc880837
Rodriguez-Xin <yangxin1545@126.com>
2017-11-12 22:23:44 +0800

    [designpattern] 添加抽象工厂模式

--- app/src/main/java/com/yangxin/designpattern/factory/abstractfactory/AbstractFactory.java
+++ app/src/main/java/com/yangxin/designpattern/factory/abstractfactory/AbstractFactory.java
@@ -1,12 +1,12 @@
 package com.yangxin.designpattern.factory.abstractfactory;
 
-import com.yangxin.designpattern.factory.AbstractProduct;
-
 /**
  * Created by xinyang on 2017/10/18.
  */
 
 public abstract class AbstractFactory {
 
-    protected abstract AbstractProduct productionProduct();
+    protected abstract AppleProduct createApple();
+
+    protected  abstract SamSungProduct createSamSung();
 }
